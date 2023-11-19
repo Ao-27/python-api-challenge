22e17330d43393625fe4788d1708cfe349d7ed6a
Ao-27 <jowrk.oldacreajani@gmail.com>
2023-11-19 11:01:41 -0500

    inval api key to be fixed

--- API_Setup/api_keys.py
+++ API_Setup/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e656826b820d6c1d17da5295908a4cf6"   #"YOUR KEY HERE"
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
